1f10f7f686061b4212b2223d0203ec84b66f13b1
Ryan Yagin <ryan.yagin@1902software.com>
2017-06-12 12:59:40 +0800

    add woo redirect

--- wp-content/themes/district123/functions.php
+++ wp-content/themes/district123/functions.php
@@ -1866,4 +1866,47 @@ function wpse_131562_redirect() {
     }
 }
 add_action('template_redirect', 'wpse_131562_redirect');
+
+add_filter('woocommerce_login_redirect', 'bryce_wc_login_redirect');
+
+function bryce_wc_login_redirect( $redirect ) {
+    if ( !is_bought_items()){
+        $redirect = '/shop';
+        return $redirect;
+    }
+}
+
+function is_bought_items() {
+
+    $bought = false;
+
+    // setting the IDs of specific products that are needed to be bought by the customer
+    // => Replace the example numbers by your specific product IDs
+    $prod_arr = array( '21', '67' );
+
+    // Get all customer orders
+    $customer_orders = get_posts( array(
+        'numberposts' => -1,
+        'meta_key'    => '_customer_user',
+        'meta_value'  => get_current_user_id(),
+        'post_type'   => 'shop_order', // WC orders post type
+        'post_status' => 'wc-completed' // Only orders with status "completed"
+    ) );
+
+    // Going through each current customer orders
+    foreach ( $customer_orders as $customer_order ) {
+        $order = wc_get_order( $customer_order );
+        // $order_id = $order->id;
+
+        // Going through each current customer products bought in the order
+        foreach ($items as $item) {
+                $bought = true; // Corrected mistake in variable name
+        }
+    }
+
+    // return "true" if one the specifics products have been bought before by customer
+    if ( $bought ) {
+        return true;
+    }
+}
 ?>
