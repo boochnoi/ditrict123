fb9d8dbc076e32ba0004efa32dd9e2a6588cde4f
Ryan Yagin <ryan.yagin@1902software.com>
2017-07-02 07:46:32 +0800

    complete order

--- wp-content/themes/district123/functions.php
+++ wp-content/themes/district123/functions.php
@@ -1961,7 +1961,7 @@ function add_order_email_instructions( $order, $sent_to_admin ) {
       // custom message for order of product subscription type
       //if( function_exists ('wcs_order_contains_subscription')){
       //    if ( wcs_order_contains_subscription ($order->ID)){
-      if (WC_Subscriptions_Order::order_contains_subscription($order_id)) {
+      if (YWSBS_Subscription_Order::order_contains_subscription($order_id)) {
       echo '<p></p>';
         echo '<p><strong>Thank you for subscribing.<br/>This will be your assigned Shipping Address:</strong></p>';
         echo '<p></p>';
