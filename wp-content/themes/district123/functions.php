0acc71b4a673c973328d1bd1cbd4d228af6288ce
Ryan Yagin <ryan.yagin@1902software.com>
2017-07-01 10:15:22 +0800

    customized my account

--- wp-content/themes/district123/functions.php
+++ wp-content/themes/district123/functions.php
@@ -1958,18 +1958,13 @@ function add_order_email_instructions( $order, $sent_to_admin ) {
   
   if ( ! $sent_to_admin ) {
  
-    if ( 'cod' == $order->payment_method ) {
-      // cash on delivery method
-      echo '<p><strong>Instructions:</strong> Full payment is due immediately upon delivery: <em>cash only, no exceptions</em>.</p>';
-    } else {
-      // other methods (ie credit card)
+      // custom message for order of product subscription type
       echo '<p><strong>Thank you for subscribing.  This will be your assigned Shipping Address:</strong></p>';
       echo '<p>Diane Brake PPS Ltd MYNZ.SHOP</p>';
       echo '<p>127 Elmore Road 0793</p>';
       echo '<p>Albany, Auckland</p>';
       echo '<p>New Zealand</p>';
       echo '<p>(64 9)414 6477</p>';
-    }
   }
 }
 ?>
