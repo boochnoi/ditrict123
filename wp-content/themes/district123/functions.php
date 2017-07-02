d30e379978aa589631611e5288ec871312adecf4
Ryan Yagin <ryan.yagin@1902software.com>
2017-07-02 09:20:34 +0800

    complete order

--- wp-content/themes/district123/functions.php
+++ wp-content/themes/district123/functions.php
@@ -1896,8 +1896,8 @@ function is_bought_items() {
     // Going through each current customer orders
     foreach ( $customer_orders as $customer_order ) {
         $order = wc_get_order( $customer_order );
-        $order_id = $order->id;
-        echo $order;
+        //$order_id = $order->id;
+        //echo $order;
 
         // Going through each current customer products bought in the order
         foreach ($items as $item) {
@@ -1915,47 +1915,6 @@ function is_bought_items() {
         return true;
     }
 }
-
-//check product if exist on specific order
-
-function is_bought_items_order() {
-    $bought = false;
-
-    // setting the IDs of specific products that are needed to be bought by the customer
-    // => Replace the example numbers by your specific product IDs
-    $prod_arr = array( '83', '84','85' );
-
-    // Get all customer orders
-    $customer_orders = get_posts( array(
-        'numberposts' => -1,
-        'meta_key'    => '_customer_user',
-        'meta_value'  => get_current_user_id(),
-        'post_type'   => 'shop_order', // WC orders post type
-        'post_status' => 'wc-completed' // Only orders with status "completed"
-    ) );
-
-    // Going through each current customer orders
-    foreach ( $customer_orders as $customer_order ) {
-        $order = wc_get_order( $customer_order );
-        // $order_id = $order->id;
-        // 
-        // Going through each current customer products bought in the order
-        foreach ($items as $item) {
-
-            // Your condition related to your 2 specific products Ids
-            if ( in_array( $item['product_id'], $prod_arr ) ) {
-
-                $bought = true; // Corrected mistake in variable name
-            }
-        }
-    }
-
-    // return "true" if one the specifics products have been bought before by customer
-    if ( $bought ) {
-        return true;
-    }
-}
-
 add_action( 'woocommerce_email_before_order_table', 'add_order_email_instructions', 10, 2 );
  
 function add_order_email_instructions( $order, $sent_to_admin ) {
