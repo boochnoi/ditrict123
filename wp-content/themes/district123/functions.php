24d6068fe21abc1edabaeddaed7b3b6915c38070
Ryan Yagin <ryan.yagin@1902software.com>
2017-07-05 04:30:10 +0800

    show address on completed order email if order has subscription product

--- wp-content/themes/district123/functions.php
+++ wp-content/themes/district123/functions.php
@@ -1959,18 +1959,19 @@ function add_order_email_instructions( $order, $sent_to_admin ) {
         global $woocommerce;
         
         $order = new WC_Order($order->ID);
-        echo $order;
-        $user_id = (int)$order->user_id;
-        $items = $order->get_items();
-           foreach ($items as $item) {
-               if ( $item['product_id' ]== 83  || $item['product_id' ]== 84  || $item['product_id' ]== 85 ){ 
-                    echo '<p></p>';
-                    echo '<p><strong>Thank you for subscribing.<br/>This will be your assigned Shipping Address:</strong></p>';
-                    echo '<p></p>';
-                    echo '<p>Diane Brake PPS Ltd MYNZ.SHOP<br/>127 Elmore Road 0793<br/>Albany, Auckland<br/>New Zealand<br/>(64 9)414 6477</p>';
-                    break;
+        if ($order->status == 'completed'){
+            $user_id = (int)$order->user_id;
+            $items = $order->get_items();
+               foreach ($items as $item) {
+                   if ( $item['product_id' ]== 83  || $item['product_id' ]== 84  || $item['product_id' ]== 85 ){ 
+                        echo '<p></p>';
+                        echo '<p><strong>Thank you for subscribing.<br/>This will be your assigned Shipping Address:</strong></p>';
+                        echo '<p></p>';
+                        echo '<p>Diane Brake PPS Ltd MYNZ.SHOP<br/>127 Elmore Road 0793<br/>Albany, Auckland<br/>New Zealand<br/>(64 9)414 6477</p>';
+                        break;
+                   }
                }
-           }
+        }
   }
 }
 
