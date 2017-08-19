173acbc45d9c6122bc1d4f270932674b71844db5
Ryan Yagin <ryan.yagin@1902software.com>
2017-08-19 08:12:10 +0800

    adjust commission fees

--- wp-content/themes/district123/functions.php
+++ wp-content/themes/district123/functions.php
@@ -2391,7 +2391,7 @@ if ( is_admin() && ! defined( 'DOING_AJAX' ) )
     } 
     
     //if cart contacins subscription product skip
-    if ($type == 'no'){
+    if ($type != 'no'){
         // Make sure that you return false here.  We can't double tax people!
         $commission = $price * 0.05;
 
@@ -2403,4 +2403,31 @@ if ( is_admin() && ! defined( 'DOING_AJAX' ) )
         $woocommerce->cart->add_fee( 'Commission', $commission, true, '' );
         $woocommerce->cart->add_fee( 'Customs Exportation Fee', $customs_fee, true, '' );
     }
+}
+// hide out of stock product
+add_action( 'pre_get_posts', 'iconic_hide_out_of_stock_products' );
+
+function iconic_hide_out_of_stock_products( $q ) {
+
+    if ( ! $q->is_main_query() || is_admin() ) {
+        return;
+    }
+
+    if ( $outofstock_term = get_term_by( 'name', 'outofstock', 'product_visibility' ) ) {
+
+        $tax_query = (array) $q->get('tax_query');
+
+        $tax_query[] = array(
+            'taxonomy' => 'product_visibility',
+            'field' => 'term_taxonomy_id',
+            'terms' => array( $outofstock_term->term_taxonomy_id ),
+            'operator' => 'NOT IN'
+        );
+
+        $q->set( 'tax_query', $tax_query );
+
+    }
+
+    remove_action( 'pre_get_posts', 'iconic_hide_out_of_stock_products' );
+
 }
