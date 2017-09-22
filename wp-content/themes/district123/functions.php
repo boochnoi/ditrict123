e56f7d22e02c8b9f34d79abc83f0d56aec651c3f
Ryan Yagin <ryan.yagin@1902software.com>
2017-09-23 07:48:59 +0800

    auto 0.00 sales

--- wp-content/themes/district123/functions.php
+++ wp-content/themes/district123/functions.php
@@ -2815,9 +2815,9 @@ function custom_override_checkout_fields( $fields ) {
 /*** automatic 0.00 sale price ***/
 function set_default_price( $post_id, $post ) {
 
-    if ( isset( $_POST['_sale_price'] ) && trim( $_POST['_sale_price'] ) == '' ) {
+    //if ( isset( $_POST['_sale_price'] ) && trim( $_POST['_sale_price'] ) == '' ) {
         update_post_meta( $post_id, '_sale_price', '0.00' );
-    }
+    //}
 
 }
 add_action( 'woocommerce_process_product_meta', 'set_default_price' );
